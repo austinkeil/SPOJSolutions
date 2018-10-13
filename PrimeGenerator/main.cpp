3998c5796fbe213f5ca68705b0c60e10562db2e4
Austin Keil <austin.c.keil@gmail.com>
2018-10-13 00:43:55 -0700

    new

--- PrimeGenerator/main.cpp
+++ PrimeGenerator/main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-using namespace std;
 
+using namespace std;
 
 
 int main() {
@@ -12,10 +12,10 @@ int main() {
 
     vector<int> *primes = new vector<int>;
     primeBits[0] = primeBits[1] = false;
-    for(int i = 2; i < SIZE; ++i){
-        if(primeBits[i]){
+    for (int i = 2; i < SIZE; ++i) {
+        if (primeBits[i]) {
             primes->push_back(i);
-            for(int j = i * i; j < SIZE; j += i){
+            for (int j = i * i; j < SIZE; j += i) {
                 primeBits[j] = false;
             }
         }
@@ -24,25 +24,23 @@ int main() {
     int t, m, n;
     cin >> t;
 
-    for(int i = 0; i < t; ++i){
+    for (int i = 0; i < t; ++i) {
         cin >> m >> n;
         vector<int>::iterator p;
 
-        for(int j = m; j <= n; ++j){
-            if(j % 6 != 1 || j % 6 != 5) {
-                bool thisPrime = true;
-                for (p = primes->begin(); *p < j && p != primes->end(); ++p) {
-                    if (j % *p == 0) {
-                        thisPrime = false;
-                        break;
-                    }
-                }
-                if (thisPrime && j > 1) {
-                    cout<<j<<"\n";
+        for (int j = m; j <= n; ++j) {
+            bool thisPrime = true;
+            for (p = primes->begin(); *p < j && p != primes->end(); ++p) {
+                if (j % *p == 0) {
+                    thisPrime = false;
+                    break;
                 }
             }
+            if (thisPrime && j > 1) {
+                cout << j << "\n";
+            }
         }
-        cout<<"\n";
+        cout << "\n";
     }
     return 0;
 }
