9b9063780d1c7386b2646e36db61f1f2b5b9ba2b
Austin Keil <austin.c.keil@gmail.com>
2018-10-21 15:21:12 -0700

    cleaner than first try

---
+++ WILLITST/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+typedef long long       ll;
+
+using namespace std;
+int main() {
+    ll n;
+    cin >> n;
+    ceil(log2(n)) == floor(log2(n)) ? cout << "TAK" << endl : cout << "NIE" << endl;
+    return 0;
+}
